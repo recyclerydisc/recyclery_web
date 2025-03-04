1d19e899ada79901bb664d00ea4175b61d002d73
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2025-03-03 20:05:12 -0600

    add support pages

---
+++ apps/frontend/src/pages/support_us/contribute-financially.tsx
@@ -0,0 +1,3 @@
+export default function ContributeFinancially() {
+  return <h1 className="text-4xl">contribute financially</h1>;
+}
---
+++ apps/frontend/src/pages/support_us/donate-time.tsx
@@ -0,0 +1,3 @@
+export default function DonateTime() {
+  return <h1 className="text-4xl">become a volunteer</h1>;
+}
