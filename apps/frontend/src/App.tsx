1e1cb346633f483683512ce4656289fef782aea6
Sophie Shin <syshin4072@gmail.com>
2025-03-03 23:47:54 -0600

    finished mapping nav-bar and routing

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { PrivateRoute, PublicOnlyRoute } from './components/protected-routes';
+import { PublicOnlyRoute } from './components/protected-routes';
 import { UserProvider } from './contexts/user-provider';
 import NavLayout from './layouts/nav-layouts';
 import Login from './pages/account/login';
 import Home from './pages/home';
 import SignUp from './pages/signup';
-
 // Pages import added
+
+import WhatWeDo from './pages/about-us/what/what';
+import WhoWeAre from './pages/about-us/who/who';
+import Classes from './pages/our-programs/classes/classes';
+import Freecyclery from './pages/our-programs/freecyclery/freecyclery';
+import FTWNB from './pages/our-programs/FTWNB/FTWNB';
+import OpenShop from './pages/our-programs/openshop/openshop';
+import ContributeFinancially from './pages/support-us/contribute-financially/contribute-financially';
+import DonateABike from './pages/support-us/donate-a-bike/donate-a-bike';
+import DonateTime from './pages/support-us/donate-time/donate-time';
+import OurSupporters from './pages/support-us/our-supporters/our-supporters';
 
 import './App.css';
 import AuthCallback from './pages/account/auth-callback';
@@ -21,9 +31,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NavLayout />}>
-            <Route element={<PrivateRoute />}>
-              <Route index element={<Home />} />
-            </Route>
+            <Route index element={<Home />} />
+            
             <Route element={<PublicOnlyRoute />}>
               <Route path="login" element={<Login />} />
               <Route path="signup" element={<SignUp />} />
@@ -32,6 +41,34 @@ function App() {
             <Route path="auth/callback" element={<AuthCallback />} />
             <Route path="auth/reset-password" element={<ResetPassword />} />
             <Route path="auth/verify-email" element={<EmailVerification />} />
+            
+            {/*
+              About Us
+            */}
+            <Route path="about-us">
+              <Route path="what" element={<WhatWeDo />} />
+              <Route path="who" element={<WhoWeAre />} />
+            </Route>
+            {/*
+              Our Programs
+            */}
+            <Route path="our-programs">
+              <Route path="classes" element={<Classes />} />
+              <Route path="freecyclery" element={<Freecyclery />} />
+              <Route path="ftwnb" element={<FTWNB />} />
+              <Route path="openshop" element={<OpenShop />} />
+            </Route>
+
+            {/*
+              Support Us
+            */}
+            <Route path="support-us">
+              <Route path="contribute-financially" element={<ContributeFinancially />} />
+              <Route path="donate-a-bike" element={<DonateABike />} />
+              <Route path="our-supporters" element={<OurSupporters />} />
+              <Route path="donate-time" element={<DonateTime />} />
+            </Route>
+
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
--- apps/frontend/src/components/navigation/nav-content.tsx
+++ apps/frontend/src/components/navigation/nav-content.tsx
@@ -6,13 +6,13 @@ export const AboutUsSubItems: NavbarSubMenuItemType[] = [
     icon: <Bike className="size-6" />,
     title: 'What We Do',
     description: 'Read about our history, mission, and values',
-    destination: '/',
+    destination: '/about-us/what',
   },
   {
     icon: <Bike className="size-6" />,
     title: 'Who We Are',
     description: 'Get to know some of our team members',
-    destination: '/',
+    destination: '/about-us/who',
   },
 ];
 
@@ -21,25 +21,25 @@ export const OurProgramsSubItems: NavbarSubMenuItemType[] = [
     icon: <Bike className="size-6" />,
     title: 'Open Shop',
     description: 'Use our tools to fix your own bike with the instruction of Recyclery mechanics',
-    destination: '/',
+    destination: '/our-programs/openshop',
   },
   {
     icon: <Bike className="size-6" />,
     title: 'Freecyclery',
     description: 'Earn a free bike through our referral and fellowship programs',
-    destination: '/',
+    destination: '/our-programs/freecyclery',
   },
   {
     icon: <Bike className="size-6" />,
     title: 'FTWN-B',
     description: 'Explore a safe space for under-represented members of the bike community',
-    destination: '/',
+    destination: '/our-programs/ftwnb',
   },
   {
     icon: <Bike className="size-6" />,
     title: 'Classes',
     description: 'Learn about the process of maintaining and overhauling your own bike',
-    destination: '/',
+    destination: '/our-programs/classes',
   },
 ];
 
@@ -48,24 +48,24 @@ export const SupportUsSubItems: NavbarSubMenuItemType[] = [
     icon: <Bike className="size-6" />,
     title: "Support Us",
     description: "Donate money to help fund The Recyclery's programs",
-    destination: "/",
+    destination: '/support-us/contribute-financially',
   },
   {
     icon: <Bike className="size-6" />,
     title: "Our Supporters",
     description: "Learn about our top supporters",
-    destination: "/",
+    destination: "/support-us/our-supporters",
   },
   {
     icon: <Bike className="size-6" />,
     title: "Donate a Bike",
     description: "Donate your old bikes to either support our programs or help people in need",
-    destination: "/",
+    destination: '/support-us/donate-a-bike',
   },
   {
     icon: <Bike className="size-6" />,
     title: "Become a volunteer",
     description: "Help improve our programs by offering your skills and expertise",
-    destination: "/",
+    destination: "/support-us/donate-time",
   },
 ];
