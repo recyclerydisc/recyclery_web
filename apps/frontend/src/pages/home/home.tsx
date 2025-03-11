b26a7ebc13cbe9eba9d7912ee20d3cc6442f2995
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2025-03-10 21:01:12 -0500

    split home page into sections in separate files, make it responsive

---
+++ apps/frontend/src/pages/home/home.tsx
@@ -0,0 +1,15 @@
+import HeroSection from './hero-section';
+import ProgramsSection from './programs-section';
+import CalendarSection from './calendar-section';
+import VideoSection from './video-section';
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ProgramsSection />
+      <CalendarSection />
+      <VideoSection />
+    </>
+  );
+}
