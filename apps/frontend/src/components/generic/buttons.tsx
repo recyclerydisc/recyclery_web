3168b9f9f53b301b36b0ad6fb16df3a39a778b24
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2025-04-07 22:14:02 -0500

    create generic components for text, buttons, and a tinted background image, and use them in the home page

---
+++ apps/frontend/src/components/generic/buttons.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+export function BlueButtonLink({ to, children }: { to: string; children: React.ReactNode }) {
+  return (
+    <Link
+      to={to}
+      className="bg-blue-500 hover:bg-blue-700 px-3 py-2 rounded-2xl text-white text-body2 cursor-pointer transition-colors font-brandon"
+    >
+      {children}
+    </Link>
+  );
+}
+
+export function OrangeButtonLink({ to, children }: { to: string; children: React.ReactNode }) {
+  return (
+    <Link
+      to={to}
+      className="bg-orange-500 hover:bg-orange-700 px-3 py-2 rounded-2xl text-white text-body2 cursor-pointer transition-colors font-brandon"
+    >
+      {children}
+    </Link>
+  );
+}
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+export function BlueButton({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 px-3 py-2 rounded-2xl text-white text-body2 cursor-pointer transition-colors font-brandon"
+    >
+      {children}
+    </button>
+  );
+}
+
+export function OrangeButton({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-orange-500 hover:bg-orange-700 px-3 py-2 rounded-2xl text-white text-body2 cursor-pointer transition-colors font-brandon"
+    >
+      {children}
+    </button>
+  );
+}
--- apps/frontend/src/components/home/program.tsx
+++ apps/frontend/src/components/home/program.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { H3 } from '../generic/styled-tags';
+import { BlueButtonLink } from '../generic/buttons';
 
 interface ProgramProps {
   children: ReactNode;
@@ -10,14 +11,9 @@ interface ProgramProps {
 export default function Program({ children, title, learnMoreLink }: ProgramProps) {
   return (
     <div className="space-y-4">
-      <h3 className="text-2xl text-orange-500">{title}</h3>
+      <H3>{title}</H3>
       <p className="text-body2 font-brandon">{children}</p>
-      <Link
-        to={learnMoreLink}
-        className="bg-blue-500 hover:bg-blue-700 px-3 py-2 rounded-2xl text-white text-body2 cursor-pointer transition-colors font-brandon"
-      >
-        Learn More
-      </Link>
+      <BlueButtonLink to={learnMoreLink}>Learn More</BlueButtonLink>
     </div>
   );
 }
