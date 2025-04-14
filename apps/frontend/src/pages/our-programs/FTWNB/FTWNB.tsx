f5d7300f8e2438c33f587f8566a593cd0d251ff6
Arthur <arttomzak@gmail.com>
2025-04-14 17:55:58 -0500

    everything except for images

--- apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
+++ apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
@@ -1,5 +1,6 @@
 //import NavBar from '../../components/navigation/nav-bar';
 //import Footer from '../../components/footer/footer';
+import { Link } from 'react-router-dom';
 
 function FTWNB() {
   return (
@@ -14,39 +15,71 @@ function FTWNB() {
         }}
       >
         <main className="max-w-screen-xl mx-auto p-4">
-          <section className="mb-8 text-center">
+          <section className="mb-16 text-center">
             <h1 className="text-3xl m-4">FTWN-B Shop</h1>
             <p className="text-3xl">for people identifying as femme, trans,</p>
             <p className="text-3xl">women, or non-binary.</p>
 
-            {/* ROUNDED CIRCLE BACKGROUND THING */}
+            {/* rounded button */}
             <div className="m-3">
-              <p className="text-xl bg-orange-200 px-3 rounded-2xl inline-block">sundays 3-6 PM</p>
+              <p className="text-xl bg-tan-500 px-3 rounded-2xl inline-block">sundays 3-6 PM</p>
             </div>
           </section>
 
-          <section className="mb-8 text-center">
-            <h2 className="text-3xl font-bold mb-2">in this program...</h2>
-            <div className="font-brandon">
-              <p className="mb-4">
-                Bring in your bike and learn some repair skills from talented mechanics
+          {/* Row one of 2x2 */}
+          <section className="flex flex-col md:flex-row items-center justify-between gap-8 mb-20 mx-20">
+            {/* Left text block */}
+            <div>
+              <h2 className="text-3xl font-bold mb-2">in this program...</h2>
+              <div className="font-brandon">
+                <p className="mb-4">
+                  Bring in your bike and learn some repair skills from talented mechanics
+                </p>
+                <p className="mb-4">
+                  Volunteer and fix up bikes for our{' '}
+                  <Link
+                    to="https://www.therecyclery.org/our-programs/freecyclery/"
+                    className="text-blue-500 underline"
+                  >
+                    Freecyclery Progam
+                  </Link>
+                </p>
+
+                <p>Hang out with fellow FTWN-B bike riders</p>
+              </div>
+            </div>
+
+            {/* image */}
+            <div>
+              <p>image</p>
+            </div>
+          </section>
+
+          {/* Row two of 2x2 */}
+          <section className="flex flex-col md:flex-row items-center justify-between gap-8 mb-20 mx-20">
+            {/* Image */}
+            <div>
+              <p>image</p>
+            </div>
+            {/* text block */}
+            <div>
+              <p className="font-brandon">
+                This program is specifically catering to the Femme, Trans, Women, and Non-Binary
+                members of our community.* We're aiming to provide a welcoming, safe space for
+                individuals who have historically been excluded by the bicycling community.
               </p>
-              <p className="mb-4">Volunteer and fix up bikes for our Freecyclery Program</p>
-              <p>Hang out with fellow FTWN-B bike riders</p>
             </div>
           </section>
 
-          <section className="mb-8 font-brandon text-center">
+          <section className="mb-4 font-brandon text-xs flex justify-end">
             <p>
-              This program is specifically catering to the Femme, Trans, Women, and Non-Binary
-              members of our community.* We're aiming to provide a welcoming, safe space for
-              individuals who have historically been excluded by the bicycling community.
-            </p>
-          </section>
-
-          <section className="mb-4 font-brandon">
-            <p>
-              *We ask for people who don't identify as FTWN-B to come to our general Open Shop Hours
+              *We ask for people who don't identify as FTWN-B to come to our general{' '}
+              <Link
+                to="https://www.therecyclery.org/our-programs/open-shop/"
+                className="text-blue-500 underline"
+              >
+                Open Shop Hours
+              </Link>{' '}
               instead
             </p>
           </section>
