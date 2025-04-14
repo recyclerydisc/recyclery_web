fc7cfc1d52b3bfb6e995e041cb19c0a42735a991
Sophie Shin <syshin4072@gmail.com>
2025-04-14 14:46:41 -0500

    setup for classes

--- apps/frontend/src/pages/our-programs/classes/classes.tsx
+++ apps/frontend/src/pages/our-programs/classes/classes.tsx
@@ -1,38 +1,47 @@
 //import NavBar from '../../components/navigation/nav-bar';
 //import Footer from '../../components/footer/footer';
+import headerPoster from '../../../assets/header-poster.jpg';
+import { H1 } from '../../../components/generic/styled-tags';
+import { BlueButtonLink, OrangeButtonLink } from '../../../components/generic/buttons';
+import { BgImage } from '../../../components/generic/bg-image';
+
 
 function Classes() {
   return (
-    <>
-      {/* <NavBar /> */}
-
+    <div>
+    <BgImage image={headerPoster} className="min-h-[32rem]">
+    {/* <NavBar /> */}
       <main className="max-w-screen-xl mx-auto p-4">
         <section className="mb-8 text-center">
-          <h1 className="text-3xl">Classes</h1>
-          <p className="text-lg">
+          <H1>Classes</H1>
+          <p className="text-heading2 pt-8 max-w-[56rem] font-brandon">
             We teach bicycle maintenance through our introductory Tune Up Class and our advanced
             Overhaul Class.
           </p>
         </section>
+        </main>
+        </BgImage> 
+        <section className="bg-background px-16 pt-10 pb-16">
 
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">mechanics 101</h2>
-          <p>text</p>
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">mechanics 101</h2>
+            <p>text</p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">overhaul class</h2>
+            <p>text</p>
+          </section>
+
+          <section>
+            <h2 className="text-xl font-semibold mb-2">sign up</h2>
+            <p>text and cards</p>
+          </section>
         </section>
-
-        <section className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">overhaul class</h2>
-          <p>text</p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">sign up</h2>
-          <p>text and cards</p>
-        </section>
-      </main>
-
-      {/* <Footer /> */}
-    </>
+      
+    </div>
+ 
+    
   );
 }
 
