2047ebc15c1d33f76f2fa252c370364d03e4f358
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2025-04-21 20:01:19 -0500

    update home hero section

--- apps/frontend/src/components/home/hero-section.tsx
+++ apps/frontend/src/components/home/hero-section.tsx
@@ -1,17 +1,17 @@
 import headerPoster from '../../assets/images/home/header-poster.jpg';
-import { H1 } from '../generic/styled-tags';
-import { BlueButtonLink, OrangeButtonLink } from '../generic/buttons';
 import { BgImage } from '../generic/bg-image';
+import { BlueButtonLink, OrangeButtonLink } from '../generic/buttons';
+import { H1 } from '../generic/styled-tags';
 
 export default function HeroSection() {
   return (
     <BgImage image={headerPoster} className="min-h-[32rem]">
       <H1>the recyclery</H1>
-      <p className="text-heading2 pt-8 max-w-[56rem] font-brandon">
+      <p className="text-body1 sm:text-heading2 pt-8 max-w-[56rem] font-brandon">
         The Recyclery Collective is an educational bike shop that promotes sustainability by giving
         access to tools, skills, and opportunities for collaboration.
       </p>
-      <div className="pt-6 space-x-4">
+      <div className="flex sm:flex-row flex-col justify-center items-center gap-x-4 gap-y-2 pt-6">
         <OrangeButtonLink to="#">Join Our Newsletter</OrangeButtonLink>
         <BlueButtonLink to="https://therecyclery.square.site/">Shop Our Bikes</BlueButtonLink>
       </div>
