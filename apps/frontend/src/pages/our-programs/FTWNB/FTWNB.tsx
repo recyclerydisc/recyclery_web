e6ea58ff93cb088876b97d86df5cb83e3b0a637e
Arthur <arttomzak@gmail.com>
2025-04-21 20:28:37 -0500

    reformatting page

--- apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
+++ apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
@@ -1,6 +1,5 @@
 //import NavBar from '../../components/navigation/nav-bar';
 //import Footer from '../../components/footer/footer';
-import transfeministSymbol from '../../../assets/images/our-programs/FTWNB/A_Transfeminist-Symbol_black-and-white.png';
 import bikerepairPic from '../../../assets/images/our-programs/FTWNB/FTWN-B.jpg';
 import { A } from '../../../components/generic/styled-tags';
 
@@ -17,54 +16,60 @@ function FTWNB() {
         }}
       >
         <main className="max-w-screen-xl mx-auto p-4">
-          <section className="mb-16 text-center">
-            <h1 className="text-3xl m-4">FTWN-B Shop</h1>
-            <p className="text-3xl">
-              for people identifying as femme, trans,
-              <br />
-              women, or non-binary.
+          <section className="mt-16 mb-32 text-center">
+            {/* for future migrate to H2 tag once color prop is made */}
+            <h1 className="text-5xl sm:text-7xl mb-4">FTWN-B Shop</h1>
+            <p className="text-3xl mb-6">
+              for people identifying as femme, trans, women, or non-binary.
             </p>
 
             {/* rounded button */}
             <div className="m-3">
-              <p className="text-xl bg-tan-500 px-3 rounded-2xl inline-block">sundays 3-6 PM</p>
+              <p className="text-3xl bg-tan-500 px-3 rounded-3xl inline-block">sundays 3-6 PM</p>
             </div>
           </section>
 
           {/* Row one of 2x2 */}
-          <section className="flex flex-col md:flex-row items-stretch justify-between gap-8 mb-15 mx-60">
+          <section className="flex flex-col md:flex-row items-stretch justify-between gap-8 mb-15 p-8">
             {/* Left text block */}
             <div>
-              <h2 className="text-3xl font-bold mb-2">in this program...</h2>
-              <div className="font-brandon">
-                <p className="mb-8">
+              <h2 className="text-4xl font-bold mb-4">in this program...</h2>
+              <div className="font-brandon text-xl">
+                <p className="mb-4">
                   Bring in your bike and learn some repair skills from talented mechanics
                 </p>
 
-                <p className="mb-8">
+                <p className="mb-4">
                   Volunteer and fix up bikes for our{' '}
                   <A to="https://www.therecyclery.org/our-programs/freecyclery/">
                     Freecyclery Progam
                   </A>
                 </p>
 
-                <p>Hang out with fellow FTWN-B bike riders</p>
+                <p className="mb-24">Hang out with fellow FTWN-B bike riders</p>
+
+                <p className="text-2xl mb-4">
+                  This program is specifically catering to the Femme, Trans, Women, and Non-Binary
+                  members of our community.*
+                </p>
+
+                <p className="text-2xl">
+                  We're aiming to provide a welcoming, safe space for individuals who have
+                  historically been excluded by the bicycling community.
+                </p>
               </div>
             </div>
 
             {/* image */}
             <div>
-              <img className="w-[400px] rounded-full" src={bikerepairPic} alt="Bike repair"></img>
+              <img className="w-[800px] rounded-full" src={bikerepairPic} alt="Bike repair"></img>
             </div>
           </section>
 
-          {/* Row two of 2x2 */}
-          <section className="flex flex-col md:flex-row items-stretch justify-between gap-20 mb-20 mx-60">
-            {/* Image */}
+          {/* <section className="flex flex-col md:flex-row items-stretch justify-between gap-50 mb-20 p-8">
             <div>
-              <img className="w-[800px]" src={transfeministSymbol} alt="Transfeminist symbol"></img>
+              <img className="w-[600px]" src={transfeministSymbol} alt="Transfeminist symbol"></img>
             </div>
-            {/* text block */}
             <div className="font-brandon text-2xl text-center">
               <p>
                 This program is specifically catering to the Femme, Trans, Women, and Non-Binary
@@ -75,7 +80,7 @@ function FTWNB() {
                 historically been excluded by the bicycling community.
               </p>
             </div>
-          </section>
+          </section> */}
 
           <section className="mb-4 font-brandon text-xs flex justify-end">
             <p>
