f41c506af46bc2bc975bf52f48bfc8ba6d3cf298
Mirai DTN <33908899+FuturrCoder@users.noreply.github.com>
2025-05-22 20:57:44 -0500

    Update FTWN-B page to make it more consistent with other pages

--- apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
+++ apps/frontend/src/pages/our-programs/FTWNB/FTWNB.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import bikerepairPic from '../../../assets/images/our-programs/FTWNB/FTWN-B.jpg';
-import { A } from '../../../components/generic/styled-tags.tsx';
+import { A, H1, H2, Section } from '../../../components/generic/styled-tags.tsx';
 
 function FTWNB() {
   const [imageURL, setImageURL] = useState<string | undefined>(undefined);
@@ -22,76 +22,65 @@ function FTWNB() {
   }, []);
 
   return (
-    <main>
-      {/* <NavBar /> */}
+    <>
       <title>FTWNB - The Recyclery</title>
-      <section
-        className="w-full"
-        style={{
-          background: `linear-gradient(rgba(225, 140, 140, 1), rgba(236, 183, 183, 1), rgba(240, 217, 194, 1))`,
-          backgroundSize: 'cover',
-        }}
-      >
-        <main className="max-w-screen-xl mx-auto p-4">
-          <section className="mt-16 mb-32 text-center">
-            {/* for future migrate to H2 tag once color prop is made */}
-            <h1 className="text-5xl sm:text-7xl mb-4">FTWN-B Shop</h1>
-            <p className="text-3xl mb-6">
-              for people identifying as femme, trans, women, or non-binary.
-            </p>
+      <Section className="flex flex-col justify-center items-center p-8 text-center min-h-[32rem]">
+        {/* for future migrate to H2 tag once color prop is made */}
+        <H1 className="!text-black">FTWN-B Shop</H1>
+        <p className="text-body1 sm:text-heading2 mt-8 max-w-[56rem] font-brandon">
+          for people identifying as femme, trans, women, or non-binary.
+        </p>
 
-            {/* rounded button */}
-            <div className="m-3">
-              <p className="text-3xl bg-tan-500 px-3 rounded-3xl inline-block">sundays 3-6 PM</p>
-            </div>
-          </section>
+        {/* rounded button */}
+        <div className="mt-8">
+          <p className="text-3xl bg-tan-500 px-3 rounded-3xl inline-block">sundays 3-6 PM</p>
+        </div>
+      </Section>
 
-          {/* Row one of text and pic */}
-          <section className="flex flex-col md:flex-row items-stretch justify-between gap-8 mb-15 mt-15 p-8">
-            {/* Left text block */}
-            <div>
-              <h2 className="text-4xl font-bold mb-4">in this program...</h2>
-              <div className="font-brandon text-xl">
-                <p className="mb-4">
-                  Bring in your bike and learn some repair skills from talented mechanics
-                </p>
+      {/* Row one of text and pic */}
+      <Section className="md:grid md:grid-cols-2 items-center gap-16 !pb-10" tan>
+        {/* Left text block */}
+        <div>
+          <H2>in this program...</H2>
+          <ul className="font-brandon text-body1 space-y-4 list-disc ml-5">
+            <li>Bring in your bike and learn some repair skills from talented mechanics</li>
 
-                <p className="mb-4">
-                  Volunteer and fix up bikes for our{' '}
-                  <A to="/our-programs/freecyclery/">Freecyclery Program</A>
-                </p>
+            <li>
+              Volunteer and fix up bikes for our{' '}
+              <A to="/our-programs/freecyclery/">Freecyclery Program</A>
+            </li>
 
-                <p className="mb-24">Hang out with fellow FTWN-B bike riders</p>
+            <li>Hang out with fellow FTWN-B bike riders</li>
+          </ul>
+        </div>
 
-                <p className="text-2xl mb-4">
-                  This program is specifically catering to the Femme, Trans, Women, and Non-Binary
-                  members of our community.*
-                </p>
+        {/* image */}
+        <div className="flex justify-center mt-6 md:mt-0">
+          <img className="max-h-64 rounded-full" src={imageURL} alt="Bike repair" />
+        </div>
+      </Section>
 
-                <p className="text-2xl">
-                  We're aiming to provide a welcoming, safe space for individuals who have
-                  historically been excluded by the bicycling community.
-                </p>
-              </div>
-            </div>
+      <Section className="font-brandon text-subheading2 space-y-4 !pt-0 !pb-16" tan>
+        <p>
+          This program is specifically catering to the Femme, Trans, Women, and Non-Binary members
+          of our community.* We're aiming to provide a welcoming, safe space for individuals who
+          have historically been excluded by the bicycling community.
+        </p>
 
-            {/* image */}
-            <div>
-              <img className="w-[800px] rounded-full" src={imageURL} alt="Bike repair"></img>
-            </div>
-          </section>
+        <p>
+          This program is an amalgamation of our programs. You can come to learn, socialize,
+          volunteer, work on your bike, and more! Our host Skylar would love to help you work on
+          your own bike or teach you skills on how to work on second hand bikes.
+        </p>
+      </Section>
 
-          <section className="mb-4 font-brandon text-xs flex justify-end">
-            <p>
-              *We ask for people who don't identify as FTWN-B to come to our general{' '}
-              <A to="/our-programs/openshop/">Open Shop Hours</A> instead
-            </p>
-          </section>
-        </main>
-      </section>
-
-      {/* <Footer /> */}
-    </main>
+      <Section className="!pt-0 !pb-8 font-brandon text-body2 flex justify-end bg-tan-500">
+        <p>
+          *We ask for people who don't identify as FTWN-B to come to our general{' '}
+          <A to="/our-programs/openshop/">Open Shop Hours</A> instead
+        </p>
+      </Section>
+    </>
   );
 }
 
