e66097b3f53af0b7442d2e481be894db664c35b1
Arthur <arttomzak@gmail.com>
2025-05-22 20:12:42 -0500

    all pages fetching pictures

--- apps/frontend/src/pages/support-us/donate-time/donate-time.tsx
+++ apps/frontend/src/pages/support-us/donate-time/donate-time.tsx
@@ -1,15 +1,51 @@
-import HeroSection from '../../../components/support-us/donate-time/hero-section.tsx';
-import VolunteerMission from '../../../components/support-us/donate-time/volunteer-mission.tsx';
-import Roles from '../../../components/support-us/donate-time/roles.tsx';
+import { useEffect, useState } from 'react';
 import headerImage from '../../../assets/images/support-us/donate-time/donate-time-header.jpg';
 import volunteerFun from '../../../assets/images/support-us/donate-time/volunteer-fun.jpg';
+import HeroSection from '../../../components/support-us/donate-time/hero-section.tsx';
+import Roles from '../../../components/support-us/donate-time/roles.tsx';
+import VolunteerMission from '../../../components/support-us/donate-time/volunteer-mission.tsx';
 
 function DonateTime() {
+  const [heroImageURL, setHeroImageURL] = useState<string | undefined>(undefined);
+  const [volunteerImageURL, setVolunteerImageURL] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    fetch(`/images/14`)
+      .then(res => res.json())
+      .then(data => {
+        if (data?.bucket_link) {
+          setHeroImageURL(data.bucket_link);
+        } else {
+          setHeroImageURL(headerImage);
+        }
+      })
+      .catch(err => {
+        console.error('image URL was not fetched correctly', err);
+        setHeroImageURL(headerImage);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`/images/15`)
+      .then(res => res.json())
+      .then(data => {
+        if (data?.bucket_link) {
+          setVolunteerImageURL(data.bucket_link);
+        } else {
+          setVolunteerImageURL(volunteerFun);
+        }
+      })
+      .catch(err => {
+        console.error('image URL was not fetched correctly', err);
+        setVolunteerImageURL(volunteerFun);
+      });
+  }, []);
+
   return (
     <>
       <title>Become a Volunteer - The Recyclery</title>
-      <HeroSection image={headerImage} />
-      <VolunteerMission image={volunteerFun} />
+      <HeroSection image={heroImageURL} />
+      <VolunteerMission image={volunteerImageURL} />
       <Roles />
     </>
   );
