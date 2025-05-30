0f6bb5aed8af6db9282342e6b5f0b4db23201928
Arthur <arttomzak@gmail.com>
2025-05-29 21:33:00 -0500

    LOCALHOST REFERENCE UPDATED TO ENV VARIABLE

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,17 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 // https://vite.dev/config/
 
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/images': {
-        target: `http://localhost:3000`, // temporary will have to modify later
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: '/',
+    server: {
+      proxy: {
+        '/images': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+
+        },
       },
-      }
+    },
   }
-});
+})
